591b0e6a437076ccfbd3efec5f54ec54e6f786c8
Nguyen Duy <wrangz@MacBook-Pro-cua-Nguyen.local>
2021-10-12 12:00:32 +0700

    Initial commit

---
+++ src/main/java/com/example/loocreative/LooCreativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loocreative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooCreativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LooCreativeApplication.class, args);
+    }
+
+}
